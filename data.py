3beb64f18e7d0c916959fedd1da2db029ae8b156
Andrey Kulchinskiy <andrii.kulchynskyi@gmail.com>
2025-11-06 18:12:42 +0300

    add test_create_user.py, conftest.py, data.py, helpers.py and upd requirements.txt

---
+++ data.py
@@ -0,0 +1,11 @@
+class Url:
+    MAIN_URL = 'https://stellarburgers.education-services.ru'
+    CREATE_USER = '/api/auth/register'
+    LOGIN_USER = '/api/auth/login'
+    USER = '/api/auth/user'
+    ORDER_CREATION = '/api/orders'
+
+class ResponseMessages:
+    USER_CREATED_SUCCESS = True
+    USER_LOGIN_ALREADY = "User already exists"
+    USER_NOT_ENOUGH_DATA = "Email, password and name are required fields"
